2e5447dcd615358dd138c58be1758d8178cb7c46
SaintpioGabo93 <121601624+SaintpioGabo93@users.noreply.github.com>
2023-06-02 02:32:19 -0600

    Curso Python Día 1
    Ejercicios

---
+++ Día 1/Proyecto_uno.py
@@ -0,0 +1,3 @@
+print('Hola, campeón vamos a buscarle nombre a tu desmadre\nPero vamos por pasos... :B')
+print('El nombre correcto de esa cosa sería: '+input('¿Cuál es el nombre de tu lugar favorito, padre? ')+
+      ' de '+input('Ahora dime cuál es tu sabor de helado favorito: '))
---
+++ Día 1/practica_print.py
@@ -0,0 +1,2 @@
+print('Hola Mundo')
+print('Hola, '+input('Escribe tu nombre: ')+'. El mundo te dice Bienvenido, :B.')
